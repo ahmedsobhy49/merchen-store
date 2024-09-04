082fe3d74b80e61e85725ec13987a5467721cd6c
Ahmed Sobhy <ahmedsobhy20012023b@gmail.com>
2024-09-04 21:33:36 +0300

    working on responsive design

--- src/pages/wishList/WishList.jsx
+++ src/pages/wishList/WishList.jsx
@@ -8,7 +8,9 @@ export default function WishList() {
   return (
     <section className="min-h-screen bg-white py-40 antialiased">
       {wishListItems.length ? (
-        <div className={`mt-14 grid grid-cols-4 gap-12 w-full p-20`}>
+        <div
+          className={`mt-8 sm:mt-14 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-5 gap-y-10 sm:gap-x-0 md:gap-x-5 md:gap-y-12 xl:gap-x-0 2xl:gap-x-5 w-full px-4 sm:px-6 md:px-8 lg:px-10 `}
+        >
           {wishListItems.map((wishListItem) => {
             return <ProductItem product={wishListItem} key={wishListItem.id} />;
           })}
